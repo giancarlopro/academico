899f1167e79a84eb9c78c68ec540820563abc138
Giancarlo Rocha <giancarloiff@gmail.com>
2020-04-29 12:48:33 -0300

    refactor: parse_xpath

--- src/academico/core/utils.py
+++ src/academico/core/utils.py
@@ -2,9 +2,9 @@ from lxml import etree
 import re
 
 def parse_xpath(content: str, xpath: str) -> str:
-    tree = etree.fromstring(content, etree.HTMLParser())
+    tree = etree.HTML(content)
 
-    return tree.xpath(xpath)[0].text
+    return tree.xpath(xpath)[0].text.strip()
 
 def parse_regex(content: str, regex: str) -> str:
     return re.search(regex, content).group(0)
