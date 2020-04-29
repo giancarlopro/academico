7ec07d90762721550858bb3ecfd89d7058db63a1
Giancarlo Rocha <giancarloiff@gmail.com>
2020-04-29 13:05:30 -0300

    add: Page class

---
+++ src/academico/core/pages.py
@@ -0,0 +1,42 @@
+import requests
+
+
+class Page:
+    url = None
+    params = None
+    headers = None
+    models = {}
+    extracted_data = {}
+
+    def __init__(self, session=None):
+        self.session = session or self.get_session()
+
+    def extract(self):
+        url = self.get_url(self.url or '')
+        params = self.get_params(self.params or {})
+        headers = self.get_headers(self.headers or {})
+
+        response = self.session.get(url, params=params, headers=headers)
+
+        for name, model in self.models.items():
+            self.extracted_data[name] = model(response.text)
+
+    def get_session(self):
+        return requests.Session()
+
+    def get_url(self, url=None):
+        return url
+
+    def get_params(self, params=None):
+        return params
+
+    def get_headers(self, headers=None):
+        return headers
+
+    def __getattr__(self, name):
+        if name in self.extracted_data:
+            return self.extracted_data[name]
+        elif name in self.models:
+            raise Exception('chame extract() antes de utilizar')
+        else:
+            raise AttributeError()
