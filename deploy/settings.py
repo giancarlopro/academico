1b060c6b893c70c03449099ab8f9bbc38ff0cbb0
Giancarlo Rocha <giancarloiff@gmail.com>
2020-09-20 10:39:15 -0300

    add token authentication

--- deploy/settings.py
+++ deploy/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'core',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -129,5 +132,11 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+AUTH_USER_MODEL = 'core.User'
--- deploy/urls.py
+++ deploy/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^api-auth/', include('rest_framework.urls'))
+    re_path(r'^api-auth/', include('rest_framework.urls')),
+    re_path(r'^authenticate/', views.obtain_auth_token),
 ]
